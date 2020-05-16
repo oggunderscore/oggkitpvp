e9f8c136aa88ea75efb8751b93ab1ed3abb77c76
Tillier1 <tillierbuisness@gmail.com>
2020-05-16 12:43:32 -0500

    Updated Ping command with readable text

--- src/me/oggunderscore/Commands/PingCommand.java
+++ src/me/oggunderscore/Commands/PingCommand.java
@@ -28,24 +28,24 @@ public class PingCommand implements CommandExecutor  {
 
                int ping = ((CraftPlayer) player).getHandle().ping;
 
-               player.sendMessage(ChatColorChange.chat("&e" + player.getDisplayName() + "'s " + "&aping:&e " + ping + "ms"));
+               player.sendMessage(ChatColorChange.chat("&7[&9Ping&7]&7 " + player.getDisplayName() + "'s ping is &e" + ping + "ms"));
 
             return true;
 
             }
-            if (args.length == 1) {
+            if (args.length == 1) {  
 
-                Player targetPlayer = Bukkit.getPlayer(args[0]);
+                Player targetPlayer = Bukkit.getPlayerExact(args[0]);
                 if (targetPlayer != null) {
 
                     int targetPing = ((CraftPlayer) targetPlayer).getHandle().ping;
 
-                    player.sendMessage(ChatColorChange.chat("&e" + targetPlayer.getDisplayName() + "'s " + "&aping:&e " + targetPing + "ms"));
+                    player.sendMessage(ChatColorChange.chat("&7[&9Ping&7]&7 " + targetPlayer.getDisplayName() + "'s ping is &e" + targetPing + "ms"));
 
 
                 } else {
 
-                    player.sendMessage("The player you entered is invalid!");
+                    player.sendMessage(ChatColorChange.chat("&7[&9Ping&7]&7 The player you entered is invalid!"));
 
                 }
 
